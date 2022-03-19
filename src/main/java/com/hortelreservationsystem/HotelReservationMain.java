90e67093f07f5b6fb12d9476f2efba23c568a740
moin21 <mamoinuddin@gmail.com>
2022-03-19 08:59:27 +0530

    Added: Welcome Message

---
+++ src/main/java/com/hortelreservationsystem/HotelReservationMain.java
@@ -0,0 +1,8 @@
+package com.hortelreservationsystem;
+
+public class HotelReservationMain {
+	public static void main(String[] args) {
+		System.out.println(" Welcome To Hotel Reservation System");
+		
+	}
+}
